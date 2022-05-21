640f758ac2b0605b743c23ab6233aa09d8c2a2c2
Abhishek32971 <abhishek32971@gmail.com>
2022-05-22 00:12:13 +0530

    fixed with sanz

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,11 +1,15 @@
-score = float(input("Enter Score: "))
-if score>=0.9:
-    print("A")
-elif score>=0.8:
-    print("B")
-elif score>=0.7:
-    print("C")
-elif score>=0.6:
-    print("D")
-elif score<0.6:
-    print("F")
+score = input("Enter Score: ")
+s=float(score)
+if(s>1 or s<0):
+    print('please enter a value between 0.0 to 1.0')
+    exit()
+if(s>=0.9):
+    print('A')
+elif(s>=0.8):
+    print('B')
+elif(s>=0.7):
+    print('C')
+elif(s>=0.6):
+    print('D')
+elif(s<0.6):
+    print('F')
