ae4100aa70f74c6889f3bbcd757a583349d2d568
Abhishek32971 <abhishek32971@gmail.com>
2022-05-24 21:10:41 +0530

    24-05-22

---
+++ ActivitySet01/test.py
@@ -0,0 +1,11 @@
+x=(input("enter the number:"))
+x=int(x)
+if (x<=80 and x>=60):
+    print(x,"is greater than 60")
+if (x<60 and x>=40):
+    print("number is greater than 40")
+if (x<40 and x>=20):
+    print("number is greater than 20")
+if (x>0 or x<20) :
+    print(x)
+print("program done")
