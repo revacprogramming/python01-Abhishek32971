2d9cb33f0071f5b9c002c3fbd791ad7bfc17394f
Abhishek32971 <abhishek32971@gmail.com>
2022-06-08 21:45:42 +0530

    8-6-22

--- ActivitySet01/test.py
+++ ActivitySet01/test.py
@@ -1,4 +1,4 @@
-txt = 'but soft what light in yonder window breaks'
+'''txt = 'but soft what light in yonder window breaks'
 words = txt.split()
 print("words:",words)
 t = list()
@@ -13,4 +13,12 @@ for length, word in t:
     res.append(word)
     print("individual res",res)
 
-print("final res:",res)
+print("final res:",res)'''
+from xml.etree.ElementTree import PI
+
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+print()
