5c04e21df1f13774e4165c1b5457358068150c91
Abhishek32971 <abhishek32971@gmail.com>
2022-07-19 16:16:42 +0530

    19-7-22

--- ActivitySet03/problem04.py
+++ ActivitySet03/problem04.py
@@ -1,4 +1,4 @@
-def inptlst():
+'''def inptlst():'''
     '''while(t):
         a=input("enter the number of values")
         x=a.split()
@@ -12,7 +12,7 @@ def inptlst():
             continue
         t-=1
     '''
-    a=input("enter the number of values")
+'''    a=input("enter the number of values")
     y=input("enter the values")
 
     try:
@@ -52,7 +52,7 @@ def finllst(x):
             i+=1
             continue
 
-    '''elif(x[i]!=0):
+    ''''''elif(x[i]!=0):
             if(x[i+1]!=0):
                 if(x[i+2]!=0):
                     y.append(x[i])
@@ -68,14 +68,14 @@ def finllst(x):
             y.append(x[i])
             i+=1
             print(y)
-    '''
-    return y
+    ''''''
+    return y'''
 
         
 
 
          
-
+'''
 def main():
     t=int(input("enter the number values you want to enter"))
     for i in range(t):
@@ -87,3 +87,76 @@ def main():
 
 
 main()
+'''
+
+
+
+
+def decode(s: list[int]):
+    res = ''
+    i = 0
+    while i < len(s):
+        if s[i] == 0:
+            if s[i + 1] == 0:
+                res += '0 '
+                i += 2
+            else:
+                n = s[i + 1]
+                c = s[i + 2]
+                for _ in range(n):
+                    res += f'{c} '
+                i += 3
+
+        else:
+            res += f'{s[i]} '
+            i += 1
+
+    return res
+
+
+def input_code():
+    while True:
+        try:
+            n = int(input().strip())
+            assert n > 0
+            break
+        except:
+            print("Enter a valid positve integer")
+            continue
+
+    inp = []
+    for _ in range(n):
+        while True:
+            try:
+                m = int(input().strip())
+                assert m > 0
+                break
+            except:
+                print("Enter a valid positive length of the code")
+                continue
+
+        while True:
+            try:
+                code = [int(i) for i in input().strip().split()]
+                if len(code) != m:
+                    print("Please enter correct number of integers")
+                    continue
+                break
+            except:
+                print("Please enter only integers")
+                continue
+
+        inp.append(code)
+
+    return inp
+
+
+def main():
+    for i in input_code():
+        print('\n')
+        print(*i)
+        print(decode(i) + '\n')
+
+
+if __name__ == "__main__":
+    main()
